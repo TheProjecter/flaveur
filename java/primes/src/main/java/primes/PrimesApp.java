c7378dcb0a6b1c5ba78dc2166cdde706485ab6d9
oleksiy.khilkevich <oleksiy.khilkevich@gmail.com>
2012-02-25 22:29:54 +0000

    primes task

---
+++ java/primes/src/main/java/primes/PrimesApp.java
@@ -0,0 +1,30 @@
+package primes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class PrimesApp {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        IPrimesFinder primesFinder = new PrimesFinder();
+        System.out.print("Enter the number: ");
+        String userInput = reader.readLine();
+        int number = Integer.parseInt(userInput);
+
+        List<Integer> primes = primesFinder.findPrimes(number);
+
+        if (! primes.isEmpty()) {
+            System.out.print("Primes: ");
+            for (int i = 0; i < primes.size() - 1; i++) {
+                System.out.print(primes.get(i) + ", ");
+            }
+            System.out.print(primes.get(primes.size() - 1) + "\n");
+        } else {
+            System.out.print("No primes found");
+        }
+    }
+}
+
